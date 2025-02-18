836ca604eae4af5a3658484005c4e3d6e43d2427
Andreas Wänqvist <andrkarl@gmail.com>
2025-02-18 14:56:41 +0100

    refactor: update SimplePrompt with improved debugging and add prompt example

--- src/backend/SKDemo.Examples/1-SimplePrompt/SimplePrompt.cs
+++ src/backend/SKDemo.Examples/1-SimplePrompt/SimplePrompt.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.SemanticKernel;
+﻿using System.Diagnostics;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
-using System.Diagnostics;
 
 namespace SKDemo.Examples
 {
@@ -11,32 +11,38 @@ namespace SKDemo.Examples
             var builder = Kernel.CreateBuilder();
 
             // Alternative using Azure OpenAI
-            //builder.AddAzureOpenAIChatCompletion(
-            // "gpt-4o-mini",                      // Azure OpenAI Deployment Name
-            // "https://contoso.openai.azure.com/", // Azure OpenAI Endpoint
-            // "Azure OpenAI Key");      // Azure OpenAI Key
-
+            // var azureFoundryKey = Environment.GetEnvironmentVariable("azureFoundryKey");
+            // Debug.Assert(
+            //     !string.IsNullOrEmpty(azureFoundryKey),
+            //     "azureFoundryKey environment variable is not set."
+            // );
+            // builder.AddAzureOpenAIChatCompletion(
+            //     "gpt-4o-mini", // Azure OpenAI Deployment Name
+            //     "https://circuitcircus6541186924.openai.azure.com/", // Azure OpenAI Endpoint
+            //     azureFoundryKey
+            // ); // Azure OpenAI Key
 
             // Alternative using OpenAI
             var openAIKey = Environment.GetEnvironmentVariable("OpenAIKey");
-            Debug.Assert(!string.IsNullOrEmpty(openAIKey), "OpenAIKey environment variable is not set.");
+            Debug.Assert(
+                !string.IsNullOrEmpty(openAIKey),
+                "OpenAIKey environment variable is not set."
+            );
             builder.AddOpenAIChatCompletion(
-                     "gpt-4o-mini",     // OpenAI Model name
-                     openAIKey);        // OpenAI API Key
+                "gpt-4o-mini", // OpenAI Model name
+                openAIKey
+            ); // OpenAI API Key
 
             var kernel = builder.Build();
 
-            var prompt = @"{{$input}}
+            var prompt =
+                @"{{$input}}
 
 You are an expert job advert copy writer. 
 You know everything about formatting a job advert so that people want to apply for the job. 
 You make awesome job adverts from rough drafts!";
 
-            var result = await kernel.InvokePromptAsync(prompt,
-                new()
-                {
-                    ["input"] = draft
-                });
+            var result = await kernel.InvokePromptAsync(prompt, new() { ["input"] = draft });
 
             //var copywriterFunction = kernel.CreateFunctionFromPrompt(prompt,
             //    executionSettings: new OpenAIPromptExecutionSettings
